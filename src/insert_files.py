5757c79fb17d638bdd8739407e3ba4331a8cec16
wbydo <wbydo@users.noreply.github.com>
2018-04-14 22:18:19 +0900

    ファイル一覧をdbに格納

---
+++ src/insert_files.py
@@ -0,0 +1,24 @@
+import os
+import glob
+
+from sqlalchemy.orm import sessionmaker
+
+from setting import ENGINE, Base, File
+
+engine = ENGINE
+Base.metadata.bind=engine
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+base = os.path.dirname(os.path.abspath(__file__))
+path = os.path.abspath(os.path.join(base, '../data/*'))
+
+for name in map(os.path.basename, glob.iglob(path)):
+    file = File()
+    file.name = name
+
+    q = session.query(File).filter_by(name=file.name)
+    if not session.query(q.exists()).scalar():
+        session.add(file)
+        session.commit()
