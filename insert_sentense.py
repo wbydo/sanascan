945776710f07eb533c3cbcde50f6188b1ec74402
wbydo <wbydo@users.noreply.github.com>
2018-04-19 23:46:19 +0900

    システム開発用にdb格納数宣言

--- insert_sentense.py
+++ insert_sentense.py
@@ -7,14 +7,19 @@ from natto import MeCab
 from setting import ENGINE, Base, Post, Sentence, Session
 from cleaner import Cleaner
 
-def iter_sentence(cleaner, mecab):
+def iter_sentence(cleaner, mecab, max=None):
     blank_ = re.compile(r'^\W*$')
     session = Session()
+    i = 0
     for post in session.query(Post).all():
         for sentence in cleaner.clean(post.contents, mecab):
             sentence_str = ' '.join(map(str, sentence))
             if not blank_.match(sentence_str):
+                if not max is None and i >= max:
+                    raise StopIteration()
+
                 print(sentence_str)
+                i += 1
                 yield {'contents':sentence_str, 'post_id':post.id, 'post_file_id':post.file_id}
 
 engine = ENGINE
@@ -23,7 +28,7 @@ Base.metadata.bind=engine
 cleaner = Cleaner()
 
 with engine.begin() as conn, MeCab() as me:
-    sentences = iter_sentence(cleaner, me)
+    sentences = iter_sentence(cleaner, me, 1000)
     insert_stmt = insert(Sentence)
     on_duplicate_key_stmt = insert_stmt.on_duplicate_key_update(
         contents=insert_stmt.inserted.contents
