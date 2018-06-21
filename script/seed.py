21a6e8d1f960eeeafdcc22565b11730326d89f45
wbydo <wbydo@users.noreply.github.com>
2018-06-21 16:53:47 +0900

    set seed.py

--- script/seed.py
+++ script/seed.py
@@ -1,6 +1,18 @@
+# ロガー設定
+from logging import getLogger,StreamHandler,DEBUG
+LOGGER = getLogger(__name__)
+LOGGER.setLevel(DEBUG)
+
+HANDLER = StreamHandler()
+HANDLER.setLevel(DEBUG)
+LOGGER.addHandler(HANDLER)
+
 #組み込みパッケージ
+import os
+import sys
 
 #サードパーティパッケージ
+from sqlalchemy import create_engine
 
 path_ = os.path.abspath(
     os.path.join(
@@ -10,5 +22,24 @@ path_ = os.path.abspath(
 sys.path.insert(0, path_)
 
 #自前パッケージ
+import sanakin.db
+from env import TARGET_DB
 
 # 定数
+DATABASE = 'mysql+pymysql://{}:{}@{}/{}?charset=utf8'.format(
+      TARGET_DB['user_name'],
+      TARGET_DB['password'],
+      TARGET_DB['host_ip'],
+      TARGET_DB['db_name']
+)
+
+ENGINE = create_engine(
+    DATABASE,
+    encoding='utf-8',
+    echo=True
+)
+
+sanakin.db.init(ENGINE)
+
+if __name__ == '__main__':
+    print('script的な処理はここで書く')
