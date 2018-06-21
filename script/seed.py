e82d636710ab5487b97a67255d075f3c97e622b0
wbydo <wbydo@users.noreply.github.com>
2018-06-21 19:52:22 +0900

    insert original_datum

--- script/seed.py
+++ script/seed.py
@@ -16,6 +16,7 @@ import hashlib
 #サードパーティパッケージ
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.dialects.mysql import insert
 
 path_ = os.path.abspath(
     os.path.join(
@@ -107,6 +108,56 @@ def insert_snkfile(file_path, corpus_id):
             LOGGER.info('')
             session.close()
 
+def insert_original_datum(corpus_symbol, file_dir):
+    def process_all_line():
+        session = Session()
+        corpus = session.query(db.Corpus).filter(
+            db.Corpus.symbol == corpus_symbol).one()
+        snkfiles = corpus.snkfiles
+        session.close()
+
+        for snkfile in snkfiles:
+            for line in snkfile.readline(RAKUTEN_TRAVEL_DIR):
+                extracted = corpus.extract_data(line)
+                result =  {
+                    'snkfile_id':snkfile.id,
+                    'corpus_symbol': corpus.symbol,
+
+                    'snkfile_id': snkfile.id,
+                    'id_in_corpus': extracted['id_in_corpus'],
+                    'contents': extracted['contents'],
+                    'symbol': ''.join([
+                        corpus.symbol,
+                        '{:0>8}'.format(extracted['id_in_corpus'])
+                    ])
+                }
+                LOGGER.info('proccess_file: ' + str(result['symbol']) + '  ' + result['contents'][:20])
+                yield result
+
+    def insert_original_datum(datum):
+        insert_stmt = insert(db.OriginalData)
+        on_duplicate_key_stmt = insert_stmt.on_duplicate_key_update(
+            snkfile_id=insert_stmt.inserted.snkfile_id,
+            id_in_corpus=insert_stmt.inserted.id_in_corpus,
+            contents=insert_stmt.inserted.contents,
+            symbol=insert_stmt.inserted.symbol,
+        )
+        with ENGINE.begin() as conn:
+            conn.execute(on_duplicate_key_stmt, datum)
+
+    datum = []
+    max_size = 500_000_000
+    for d in process_all_line():
+        if sys.getsizeof(datum) < max_size:
+            datum.append(d)
+        else:
+            LOGGER.info(f'INSERT: {len(datum)}件挿入!!!')
+            insert_original_datum(datum)
+            datum = []
+    if datum:
+        LOGGER.info(f'INSERT: {len(datum)}件挿入!!!')
+        insert_original_datum(datum)
+
 if __name__ == '__main__':
     # 楽天データに特化した処理を記載
     insert_corpus(
@@ -120,3 +171,5 @@ if __name__ == '__main__':
         if fnmatch.fnmatch(file_name, 'travel02_userReview[0-9]*'):
             file_path = os.path.join(RAKUTEN_TRAVEL_DIR, file_name)
             insert_snkfile(file_path, c.id)
+
+    insert_original_datum('RTUR', RAKUTEN_TRAVEL_DIR)
