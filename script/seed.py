c08258d7301fb3519f84415862993da0df8b1209
wbydo <wbydo@users.noreply.github.com>
2018-06-30 18:10:42 +0900

    command: delete

--- script/seed.py
+++ script/seed.py
@@ -1,23 +1,12 @@
-# ロガー設定
-from logging import getLogger,StreamHandler,DEBUG
-LOGGER = getLogger('sanakin.corpus_data.cli')
-LOGGER.setLevel(DEBUG)
-
-HANDLER = StreamHandler()
-HANDLER.setLevel(DEBUG)
-LOGGER.addHandler(HANDLER)
-
 #組み込みパッケージ
 import os
 import sys
-import fnmatch
-import hashlib
 from contextlib import contextmanager
+import argparse
 
 #サードパーティパッケージ
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.dialects.mysql import insert
 
 path_ = os.path.abspath(
     os.path.join(
@@ -31,25 +20,27 @@ import sanakin
 import sanakin.corpus.cli as corpus
 import sanakin.corpus_file.cli as corpus_file
 import sanakin.corpus_data.cli as corpus_data
+
 from env import TARGET_DB, RAKUTEN_TRAVEL_DIR
-
-# 定数
-DATABASE = 'mysql+pymysql://{}:{}@{}/{}?charset=utf8'.format(
-      TARGET_DB['user_name'],
-      TARGET_DB['password'],
-      TARGET_DB['host_ip'],
-      TARGET_DB['db_name']
-)
-
-ENGINE = create_engine(
-    DATABASE,
-    encoding='utf-8',
-    echo=True
-)
 
 @contextmanager
 def Session():
-    _Session = sessionmaker(bind=ENGINE)
+    database = 'mysql+pymysql://{}:{}@{}/{}?charset=utf8'.format(
+          TARGET_DB['user_name'],
+          TARGET_DB['password'],
+          TARGET_DB['host_ip'],
+          TARGET_DB['db_name']
+    )
+
+    engine = create_engine(
+        database,
+        encoding='utf-8',
+        echo=True
+    )
+
+    sanakin.init(engine)
+
+    _Session = sessionmaker(bind=engine)
     session = _Session()
     try:
         yield session
@@ -59,23 +50,30 @@ def Session():
     finally:
         session.close()
 
-sanakin.init(ENGINE)
-
 if __name__ == '__main__':
-    with Session() as session:
-        # corpus.insert(
-        #     session,
-        #     '楽天データセット::楽天トラベル::ユーザレビュー',
-        #     'RTUR',
-        # )
+    parser = argparse.ArgumentParser(
+            description='''\
+                DBに初期データを投入するためのCLI。
+                引数なしで実行した場合、開発モードとしてRTURのサブセットをinsert。\
+            ''',
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument(
+        '-d', '--delete',
+        action='store_true',
+        help='RTURのすべてのデータを削除'
+    )
+    args = parser.parse_args()
 
-        # c = session.query(sanakin.Corpus).one()
+    if args.delete:
+        while True:
+            ans = input('RTURのすべてのデータを削除しますか？[Y/n] ')
+            if ans in ['Y', 'n']:
+                break
 
-        # file_name = 'travel02_userReview00_20160304.txt'
-        # file_path = os.path.join(RAKUTEN_TRAVEL_DIR, file_name)
-        # corpus_file.insert(session, file_path, c.corpus_id)
+        if ans == 'Y':
+            with Session() as session:
+                corpus.delete(session, 'RTUR')
 
-        # 選べるようにする
-        # corpus_data.insert(session, ENGINE, c.corpus_id, RAKUTEN_TRAVEL_DIR)
-
-        corpus.delete(session, 'RTUR')
+    else:
+        pass
