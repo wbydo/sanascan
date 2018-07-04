f9845eb7d8ccba33850fab76553fdfecfb293219
wbydo <wbydo@users.noreply.github.com>
2018-07-04 14:19:27 +0900

    add Morph insert delete

--- script/exp.py
+++ script/exp.py
@@ -11,6 +11,7 @@ path_ = os.path.abspath(
 sys.path.insert(0, path_)
 
 import sanakin.morphological_analysis.cli as manalysis
+import sanakin.morpheme.cli as morph
 from sanakin.cli_util import SNKCLIEngine
 
 # ロガー設定
@@ -36,13 +37,13 @@ class ExpEngine(SNKCLIEngine):
     @SNKCLIEngine.confirm(msg=f'{_work}:消去しますか？')
     def _delete_mode(self, session):
         manalysis.delete(session)
+        morph.delete(session)
         q = 'ALTER TABLE {} AUTO_INCREMENT = 1;'
-        for t in ['morphological_analysies']:
+        for t in ['morphological_analysies', 'morphemes']:
             session.execute(q.format(t))
 
     def _sandbox_mode(self, session):
-        import sanakin
-        sanakin.Morpheme
+        pass
 
     def _non_wrapped_insert_mode(self, session, *, is_develop_mode=True):
         with MeCab() as mecab:
@@ -51,6 +52,7 @@ class ExpEngine(SNKCLIEngine):
                 mecab,
                 is_develop_mode=is_develop_mode
             )
+        morph.insert(session, is_develop_mode=is_develop_mode)
 
     @SNKCLIEngine.confirm(msg=f'{_work}:時間がかかりますがいいですか？')
     def _long_time_insert_mode(self, session, *, is_develop_mode=True):
