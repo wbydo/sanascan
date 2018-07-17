b5706a7f5c140735f8e1ea534206e3043bc693d3
wbydo <wbydo@users.noreply.github.com>
2018-07-18 00:03:33 +0900

    session.closeのタイミング調整

--- sanakin/corpus_data/base_corpus_data.py
+++ sanakin/corpus_data/base_corpus_data.py
@@ -17,16 +17,16 @@ class BaseCorpusData:
             corpus_file = session.merge(corpus_file)
             corpus = corpus_file.corpus
 
-            for line in corpus_file.readline(dir_):
-                extracted = corpus.extract_data(line)
-                k = klass(
-                    corpus_data_id=''.join([
-                        corpus.corpus_id,
-                        '{:0>8}'.format(extracted['id_in_corpus'])
-                    ]),
-                    corpus_file_id=corpus_file.corpus_file_id,
-                    id_in_corpus=extracted['id_in_corpus'],
-                    text=extracted['text'],
-                )
-                LOGGER.info('proccess_file: ' + k.corpus_data_id + '  ' + k.text[:20])
-                yield k
+        for line in corpus_file.readline(dir_):
+            extracted = corpus.extract_data(line)
+            k = klass(
+                corpus_data_id=''.join([
+                    corpus.corpus_id,
+                    '{:0>8}'.format(extracted['id_in_corpus'])
+                ]),
+                corpus_file_id=corpus_file.corpus_file_id,
+                id_in_corpus=extracted['id_in_corpus'],
+                text=extracted['text'],
+            )
+            LOGGER.info('proccess_file: ' + k.corpus_data_id + '  ' + k.text[:20])
+            yield k
