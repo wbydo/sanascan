0470e5de5f86a53e2e0bd67d02303696e4aa180d
wbydo <wbydo@users.noreply.github.com>
2018-10-19 18:36:54 +0900

    reactでrenderした要素の描画に成功

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const path = require('path');
+
+/*
+ * SplitChunksPlugin is enabled by default and replaced
+ * deprecated CommonsChunkPlugin. It automatically identifies modules which
+ * should be splitted of chunk by heuristics using module duplication count and
+ * module category (i. e. node_modules). And splits the chunks…
+ *
+ * It is safe to remove "splitChunks" from the generated configuration
+ * and was added as an educational example.
+ *
+ * https://webpack.js.org/plugins/split-chunks-plugin/
+ *
+ */
+
+module.exports = {
+	mode: 'development',
+	plugins: [],
+
+  module: {
+      rules: [{
+          test: /\.tsx?$/,
+          use: 'ts-loader'
+      }, {
+          test: /\.tsx?$/,
+          enforce: 'pre',
+          loader: 'tslint-loader',
+          options: {
+              configFile: './tslint.json',
+              typeCheck: true,
+          },
+      }],
+  },
+
+  resolve: {
+      extensions: [
+          '.ts',
+          '.tsx',
+          '.js',
+      ]
+  },
+
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendors: {
+					priority: -10,
+					test: /[\\/]node_modules[\\/]/
+				}
+			},
+
+			chunks: 'async',
+			minChunks: 1,
+			minSize: 30000,
+			name: false
+		}
+	}
+};
