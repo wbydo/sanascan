41f1e31bf34612e4aadf1ae13de459af4c8a7158
wbydo <wbydo@users.noreply.github.com>
2018-10-25 01:25:05 +0900

    中間クラス削除

--- src/lib/App.tsx
+++ src/lib/App.tsx
@@ -1,17 +1,22 @@
 import * as React from 'react';
 
-import OutputArea from './OutputArea'
-import Buttons from './Buttons'
+import Cell from './Cell'
 
 import * as styles from './App.css'
+
+const labels: string[] = [
+  "7", "8", "9", "＋",
+  "4", "5", "6", "−",
+  "1", "2", "3", "×",
+  "0", ".", "=", "÷"
+];
 
 export default class App extends React.Component<{}, {}> {
   public render() {
     return(
       <div className={styles.frame}>
         <div className={styles.inner}>
-          {/* <OutputArea /> */}
-          <Buttons />
+          {labels.map((l) => <Cell label={l} />)}
         </div>
       </div>
     );
