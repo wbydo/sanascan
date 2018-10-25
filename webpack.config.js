1313dca2d66de46007d85ca25b322ffcc91097de
wbydo <wbydo@users.noreply.github.com>
2018-10-25 23:01:48 +0900

    インデント調整

--- webpack.config.js
+++ webpack.config.js
@@ -2,75 +2,75 @@ const webpack = require('webpack');
 const path = require('path');
 
 /*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
- const main = {
- 	mode: 'development',
-   target: 'electron-main',
-   entry: path.join(__dirname, 'src','index'),
+* SplitChunksPlugin is enabled by default and replaced
+* deprecated CommonsChunkPlugin. It automatically identifies modules which
+* should be splitted of chunk by heuristics using module duplication count and
+* module category (i. e. node_modules). And splits the chunks…
+*
+* It is safe to remove "splitChunks" from the generated configuration
+* and was added as an educational example.
+*
+* https://webpack.js.org/plugins/split-chunks-plugin/
+*
+*/
+const main = {
+  mode: 'development',
+  target: 'electron-main',
+  entry: path.join(__dirname, 'src','index'),
 
-   output: {
-     filename: 'index.js',
-     path: path.resolve(__dirname, 'dist')
-   },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
 
   node: {
     __dirname: false,
     __filename: false
   },
 
- 	plugins: [],
+  plugins: [],
 
-   module: {
-       rules: [{
-           test: /\.ts?$/,
-           use: 'ts-loader'
-       }, {
-           test: /\.ts?$/,
-           enforce: 'pre',
-           loader: 'tslint-loader',
-           options: {
-               configFile: './tslint.json',
-               typeCheck: true,
-           },
-       }],
-   },
+  module: {
+    rules: [{
+      test: /\.ts?$/,
+      use: 'ts-loader'
+    }, {
+      test: /\.ts?$/,
+      enforce: 'pre',
+      loader: 'tslint-loader',
+      options: {
+        configFile: './tslint.json',
+        typeCheck: true,
+      },
+    }],
+  },
 
-   resolve: {
-       extensions: [
-           '.ts',
-           '.js',
-       ]
-   },
+  resolve: {
+    extensions: [
+      '.ts',
+      '.js',
+    ]
+  },
 
- 	optimization: {
- 		splitChunks: {
- 			cacheGroups: {
- 				vendors: {
- 					priority: -10,
- 					test: /[\\/]node_modules[\\/]/
- 				}
- 			},
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/
+        }
+      },
 
- 			chunks: 'async',
- 			minChunks: 1,
- 			minSize: 30000,
- 			name: false
- 		}
- 	}
- };
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: false
+    }
+  }
+};
 
 const renderer = {
-	mode: 'development',
+  mode: 'development',
   target: 'electron-renderer',
   entry: path.join(__dirname, 'src', 'renderer','index'),
 
@@ -79,49 +79,49 @@ const renderer = {
     path: path.resolve(__dirname, 'dist', 'scripts')
   },
 
-	plugins: [],
+  plugins: [],
 
   module: {
-      rules: [{
-          test: /\.tsx?$/,
-          use: 'ts-loader'
-      }, {
-          test: /\.tsx?$/,
-          enforce: 'pre',
-          loader: 'tslint-loader',
-          options: {
-              configFile: './tslint.json',
-              typeCheck: true,
-          },
-      }, {
-          test: /\.css$/,
-          loaders: ['style-loader', 'css-loader?modules', 'postcss-loader'],
-      }],
+    rules: [{
+      test: /\.tsx?$/,
+      use: 'ts-loader'
+    }, {
+      test: /\.tsx?$/,
+      enforce: 'pre',
+      loader: 'tslint-loader',
+      options: {
+        configFile: './tslint.json',
+        typeCheck: true,
+      },
+    }, {
+      test: /\.css$/,
+      loaders: ['style-loader', 'css-loader?modules', 'postcss-loader'],
+    }],
   },
 
   resolve: {
-      extensions: [
-          '.ts',
-          '.tsx',
-          '.js',
-      ]
+    extensions: [
+      '.ts',
+      '.tsx',
+      '.js',
+    ]
   },
 
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				vendors: {
-					priority: -10,
-					test: /[\\/]node_modules[\\/]/
-				}
-			},
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/
+        }
+      },
 
-			chunks: 'async',
-			minChunks: 1,
-			minSize: 30000,
-			name: false
-		}
-	}
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: false
+    }
+  }
 };
 
 module.exports = [
