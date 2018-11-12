6198da805fbee139dc84eb5c433b57157c068e64
wbydo <wbydo@users.noreply.github.com>
2018-11-12 19:40:26 +0900

    mypy適用 + インデント調整

--- sanascan_backend/lang_model.py
+++ sanascan_backend/lang_model.py
@@ -1,6 +1,7 @@
 import re
 from collections import namedtuple
-
+from typing import NamedTuple, Dict, Optional, List
+from typing import Match, Pattern, ClassVar, KeysView
 from enum import Enum
 from enum import auto
 
@@ -9,71 +10,87 @@ class ArpaArea(Enum):
         NGRAM = auto()
 
 class NgramError(Exception):
-        pass
+    pass
+
+class ParseError(Exception):
+    pass
 
 class LangModel:
-        Data = namedtuple('Data', ['prob', 'backoff'])
+    class Data(NamedTuple):
+        prob: float
+        backoff: Optional[float]
 
-        remove_head = re.compile(r'^(?:.*?) (?P<target>.*)$')
-        remove_tail = re.compile(r'^(?P<target>.*) (?:.*?)$')
+    remove_head: ClassVar[Pattern] = re.compile(r'^(?:.*?) (?P<target>.*)$')
+    remove_tail: ClassVar[Pattern] = re.compile(r'^(?P<target>.*) (?:.*?)$')
 
-        def __init__(self, arpa_text):
-                self._dic = self._process_arpa_file(arpa_text)
-                self._keys = self._dic.keys()
+    _dic: Dict[str, Data]
+    _keys: KeysView[str]
 
-        def _process_arpa_file(self, arpa_text):
-                title = re.compile(r'^\\(\d)-grams:$')
-                area = ArpaArea.DATA
-                n_gram = None
+    def __init__(self, arpa_text: str) -> None:
+        self._dic = self._process_arpa_file(arpa_text)
+        self._keys = self._dic.keys()
 
-                result = {}
-                for unstriped_line in arpa_text.split('\n'):
-                        line = unstriped_line.strip()
-                        m = title.match(line)
-                        if m:
-                                area = ArpaArea.NGRAM
-                                ngram = int(m.group(1))
-                                continue
+    def _process_arpa_file(self, arpa_text: str) -> Dict[str, Data]:
+        title = re.compile(r'^\\(\d)-grams:$')
+        area = ArpaArea.DATA
+        n_gram = None
 
-                        if area == ArpaArea.DATA or line == '' or line == '\\end\\':
-                                continue
+        result = {}
+        for unstriped_line in arpa_text.split('\n'):
+            line = unstriped_line.strip()
+            m = title.match(line)
+            if m:
+                    area = ArpaArea.NGRAM
+                    ngram = int(m.group(1))
+                    continue
 
-                        data_line = line.split('\t')
-                        prob = float(data_line[0])
-                        word = data_line[1]
-                        backoff = float(data_line[2]) if len(data_line) == 3 else None
-                        result[word] = LangModel.Data(prob=prob, backoff=backoff)
-                self._order = ngram
-                return result
+            if area == ArpaArea.DATA or line == '' or line == '\\end\\':
+                    continue
 
-        def _split(self, words):
-                return words.split(' ')
+            data_line = line.split('\t')
+            prob = float(data_line[0])
+            word = data_line[1]
+            backoff = float(data_line[2]) if len(data_line) == 3 else None
+            result[word] = LangModel.Data(prob=prob, backoff=backoff)
+        self._order = ngram
+        return result
 
-        def _join(self, words):
-                return ' '.join(words)
+    def _split(self, words: str) -> List[str]:
+        return words.split(' ')
 
-        def _remove_head(self, words):
-                return LangModel.remove_head.search(words)['target']
+    def _join(self, words: str) -> str:
+        return ' '.join(words)
 
-        def _remove_tail(self, words):
-                m = LangModel.remove_tail.search(words)
-                return m['target']
+    def _remove_head(self, words: str) -> str:
+        m: Optional[Match[str]] = LangModel.remove_head.search(words)
+        if m:
+            return m['target']
+        else:
+            raise ParseError()
 
-        def score(self, words_arg):
-                words = self._split(words_arg)
-                l = len(words)
-                if l > self._order:
-                        raise NgramError(words_arg)
+    def _remove_tail(self, words: str) -> str:
+        m: Optional[Match[str]] = LangModel.remove_tail.search(words)
+        if m:
+            return m['target']
+        else:
+            raise ParseError()
 
-                if words_arg in self._keys:
-                        return self._dic[words_arg].prob
-                else:
-                        preceed = self._remove_tail(words_arg)
-                        reduced = self._remove_head(words_arg)
-                        p = self.score(reduced)
+    def score(self, words_arg: str) -> float:
+        words = self._split(words_arg)
+        l = len(words)
+        if l > self._order:
+            raise NgramError(words_arg)
 
-                        #and以下のもの付けたけどいいんかこれ
-                        if preceed in self._keys and self._dic[preceed].backoff:
-                                return self._dic[preceed].backoff + p
-                        else:
-                                return p
+        if words_arg in self._keys:
+            return self._dic[words_arg].prob
+        else:
+            preceed = self._remove_tail(words_arg)
+            reduced = self._remove_head(words_arg)
+            p = self.score(reduced)
+
+            #and以下のもの付けたけどいいんかこれ
+            backoff: Optional[float] = self._dic[preceed].backoff
+            if preceed in self._keys and (backoff is not None):
+                return backoff + p
+            else:
+                return p
---
+++ test/test_lang_model.py
@@ -0,0 +1,16 @@
+import unittest
+
+from pathlib import Path
+
+from sanascan_backend.lang_model import LangModel
+
+class TestLangModel(unittest.TestCase):
+    def setUp(self):
+        with (Path.home() / 'arpa/LM0006.txt').open() as f:
+            self.lm = LangModel(f.read())
+
+    def test_getvocab(self):
+        self.lm.get_vocab()
+
+if __name__ == '__main__':
+    unittest.main()
