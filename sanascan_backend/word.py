4c3a495d202bd0c6f72196cf496050116c408eae
wbydo <wbydo@users.noreply.github.com>
2018-11-13 01:02:20 +0900

    Wordクラスリファクタリング完了

--- sanascan_backend/word.py
+++ sanascan_backend/word.py
@@ -1,32 +1,36 @@
 from typing import NamedTuple, Dict, List
-from typing import ClassVar
+from typing import overload
+
+DELIMITER: str = '/'
+MARK: Dict[str, str] = {'unk':'<unk>', 'eng':'<eng>', 'num':'<num>'}
 
 class Word(NamedTuple):
     surface: str
     yomi: str
 
-    DELIMITER: ClassVar[str] = '/'
-    MARK: Dict[str, str] = {'unk':'<unk>', 'eng':'<eng>', 'num':'<num>'}
+    @staticmethod
+    def from_str_of_multiword(sentence: str) -> 'List[Word]':
+        return [Word.from_str_of_singleword(w) for w in sentence.split(' ')]
 
     @staticmethod
-    def from_sentence(sentence: str) -> List[Word]:
-        return [Word.from_str(w) for w in sentence.split(' ')]
+    def from_str_of_singleword(arg: str) -> 'Word':
+        if arg in MARK.values():
+            return Word(surface=arg, yomi=arg)
 
-    @staticmethod
-    def from_str(str_: str) -> Word:
-        if str_ in Word.MARK.values():
-            return Word(surface=str_, yomi=str_)
-
-        list_ = str_.split(Word.DELIMITER)
+        list_ = arg.split(DELIMITER)
         if len(list_) >= 3:
             raise ValueError
 
         return Word(surface=list_[0], yomi=list_[1])
 
+    @staticmethod
+    def to_str(words: 'List[Word]') -> str:
+        return ' '.join([str(w) for w in words])
+
     def __str__(self) -> str:
-        if self.surface in Word.MARK.values():
+        if self.surface in MARK.values():
             return self.surface
-        return f'{self.surface}{Word.DELIMITER}{self.yomi}'
+        return f'{self.surface}{DELIMITER}{self.yomi}'
 
     def __repr__(self) -> str:
         return f'Word(\'{self.surface}\',\'{self.yomi}\')'
