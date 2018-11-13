85129de88b3038c940dcb96201e3fc750c8a504b
wbydo <wbydo@users.noreply.github.com>
2018-11-14 00:17:17 +0900

    confirm test code

--- tests/test_estimator.py
+++ tests/test_estimator.py
@@ -1,9 +1,11 @@
 import unittest
 
+
 class TestEstimator(unittest.TestCase):
 
     def test_hoge(self) -> None:
         self.assertEqual(5, 5)
+
 
 if __name__ == '__main__':
     unittest.main()
