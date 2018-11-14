6d95ef09cc4f8e8bede7a3ef9cac52f18d8cd25d
wbydo <wbydo@users.noreply.github.com>
2018-11-15 01:56:45 +0900

    remove optional type in node.py

--- sanascan_backend/node.py
+++ sanascan_backend/node.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List
 
 from .lang_model import LangModel
 from .word import Word, TagWord
@@ -6,10 +6,9 @@ from .word import Word, TagWord
 
 class Node:
     _word: Word
-
-    score: Optional[float]
-    parent: 'Optional[Node]'
-    sentence: Optional[List[Word]]
+    score: float
+    parent: 'Node'
+    sentence: List[Word]
 
     def __init__(
             self,
@@ -24,33 +23,11 @@ class Node:
         scores = [f(can, lang_model) for can in candidates]
 
         max_score = max(scores)
-        self._set_score(max_score)
+        self.score = max_score
+        self.parent = candidates[scores.index(max_score)]
+        self.sentence = self.parent.sentence + [self._word]
 
-        parent = candidates[scores.index(max_score)]
-        self._set_parent(parent)
-
-    def _set_score(self, score: float) -> None:
-        self.score = score
-
-    def _set_parent(self, parent: 'Node') -> None:
-        self._parent = parent
-
-        if parent.sentence is None:
-            raise ValueError('parent.sentence is None')
-
-        self.sentence = parent.sentence + [self._word]
-
-    def _calc_score(
-            self,
-            other: 'Node',
-            lm: LangModel,
-            ) -> float:
-
-        if other.score is None:
-            raise ValueError('other.score is None')
-        if other.sentence is None:
-            raise ValueError('other.sentence is None')
-
+    def _calc_score(self, other: 'Node', lm: LangModel) -> float:
         ngram = other.sentence[-(lm.order - 1):] + [self._word]
         return other.score + lm.score(ngram)
 
@@ -58,7 +35,6 @@ class Node:
 class RootNode(Node):
     def __init__(self) -> None:
         word = TagWord('<s>')
-
         self._word = word
         self.sentence = [word]
         self.score = 0.0
@@ -66,10 +42,8 @@ class RootNode(Node):
 
 class EOSNode(Node):
     def __init__(
-            self,
-            candidates: 'List[Node]',
-            lang_model: LangModel,
-            ) -> None:
+            self, candidates: 'List[Node]',
+            lang_model: LangModel) -> None:
 
         super(self.__class__, self).__init__(
             TagWord('</s>'),
