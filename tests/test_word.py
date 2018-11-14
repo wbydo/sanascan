e41586a20aaa32b39ddae6e7774c61630ecf5130
wbydo <wbydo@users.noreply.github.com>
2018-11-14 16:12:18 +0900

    テスト追加

--- tests/test_word.py
+++ tests/test_word.py
@@ -1,4 +1,5 @@
 import unittest
+from natto import MeCab
 
 from sanascan_backend.word import Word
 
@@ -32,6 +33,40 @@ class TestWord(unittest.TestCase):
         ]
 
         self.assertEqual(Word.to_str(words), '歩下/ホゲ 普が/フガ 日余/ピヨ')
+
+    def test_from_sentence(self):
+        r1 = list(Word.from_sentence(
+            'ホテル内の飲食店が充実しており、特に１Ｆのバーは重厚なインテリアで、雰囲気が良く最高',
+            MeCab()
+        ))
+
+        r2 = [
+            Word('ホテル', 'ホテル'),
+            Word('内', 'ナイ'),
+            Word('の', 'ノ'),
+            Word('飲食', 'インショク'),
+            Word('店', 'テン'),
+            Word('が', 'ガ'),
+            Word('充実', 'ジュウジツ'),
+            Word('し', 'シ'),
+            Word('て', 'テ'),
+            Word('おり', 'オリ'),
+            Word('特に', 'トクニ'),
+            Word('<num>', '<num>'),
+            Word('<eng>', '<eng>'),
+            Word('の', 'ノ'),
+            Word('バー', 'バー'),
+            Word('は', 'ハ'),
+            Word('重厚', 'ジュウコウ'),
+            Word('な', 'ナ'),
+            Word('インテリア', 'インテリア'),
+            Word('で', 'デ'),
+            Word('雰囲気', 'フンイキ'),
+            Word('が', 'ガ'),
+            Word('良く', 'ヨク'),
+            Word('最高', 'サイコウ')
+        ]
+        self.assertEqual(r1, r2)
 
 
 if __name__ == '__main__':
