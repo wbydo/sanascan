d555955c008ccd20358edcb19f324c44462e1dc8
wbydo <wbydo@users.noreply.github.com>
2018-11-15 00:22:01 +0900

    LangModel._dicのキーをstrから変更

--- sanascan_backend/lang_model.py
+++ sanascan_backend/lang_model.py
@@ -1,5 +1,5 @@
 import re
-from typing import NamedTuple, Dict, List, Set
+from typing import NamedTuple, Dict, Set, Tuple, Iterable
 from enum import Enum
 from enum import auto
 from itertools import chain
@@ -25,12 +25,16 @@ class LangModel:
         prob: float
         backoff: float
 
-    _dic: Dict[str, Data]
+    _dic: Dict[Tuple[Word, ...], Data]
 
     def __init__(self, arpa_text: str) -> None:
         self._dic = self._process_arpa_file(arpa_text)
 
-    def _process_arpa_file(self, arpa_text: str) -> Dict[str, Data]:
+    def _process_arpa_file(
+            self,
+            arpa_text: str
+            ) -> Dict[Tuple[Word, ...], Data]:
+
         title = re.compile(r'^\\(\d)-grams:$')
         area = ArpaArea.DATA
 
@@ -48,24 +52,25 @@ class LangModel:
 
             data_line = line.split('\t')
             prob = float(data_line[0])
-            word = data_line[1]
+            word = tuple(Word.from_wakachigaki(data_line[1]))
             backoff = float(data_line[2]) if len(data_line) == 3 else 0
             result[word] = LangModel.Data(prob=prob, backoff=backoff)
         self._order = ngram
         return result
 
-    def score(self, words: List[Word]) -> float:
+    def score(self, words: Iterable[Word]) -> float:
+        words = tuple(words)
         len_ = len(words)
         if len_ > self._order:
             raise NgramError(Word.to_str(words))
 
-        if len_ == 1 and (not str(words[0]) in self._dic.keys()):
-            raise NgramError(str(words[0]) + 'は使用言語モデルの語彙にない')
+        if len_ == 1 and (words not in self._dic.keys()):
+            raise NgramError(str(words) + 'は使用言語モデルの語彙にない')
 
-        if Word.to_str(words) in self._dic.keys():
-            return self._dic[Word.to_str(words)].prob
+        if words in self._dic.keys():
+            return self._dic[words].prob
         else:
-            context = Word.to_str(words[:-1])
+            context = words[:-1]
             p = self.score(words[1:])
 
             if context in self._dic.keys():
@@ -75,6 +80,4 @@ class LangModel:
                 return p
 
     def get_vocab(self) -> Set[Word]:
-        return set(chain.from_iterable(
-            [Word.from_wakachigaki(k) for k in self._dic.keys()]
-        ))
+        return set(chain.from_iterable(self._dic.keys()))
