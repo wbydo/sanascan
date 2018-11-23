5b8893ade4f54d841052870e030dcbb255d3837b
wbydo <wbydo@users.noreply.github.com>
2018-11-23 17:10:56 +0900

    modity resource type

--- stubs/falcon/api.pyi
+++ stubs/falcon/api.pyi
@@ -1,3 +1,12 @@
 from typing import Any
+from typing_extensions import Protocol
+
+from .request import Request as Request
+from .response import Response as Response
+
+
+class OnGetResource(Protocol):
+    def on_get(self, req: Request, resp: Response) -> None: ...
+
 class API(object):
-    def add_route(self, uri_template: str, resource: Any, *args: Any, **kwargs: Any) -> None: ...
+    def add_route(self, uri_template: str, resource: OnGetResource, *args: Any, **kwargs: Any) -> None: ...
