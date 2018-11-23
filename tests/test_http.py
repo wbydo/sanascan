bd034c5ef4206e51513091f121289c26878bb006
wbydo <wbydo@users.noreply.github.com>
2018-11-23 21:46:10 +0900

    仕様を想定したテストコード追加（まだ動かない）

--- tests/test_http.py
+++ tests/test_http.py
@@ -1,8 +1,11 @@
 import unittest
 
 from falcon import testing
+from natto import MeCab
 
 from sanascan_backend.http import api
+from sanascan_backend.word import Word
+from sanascan_backend.key import Key
 
 
 class TestHTTP(testing.TestCase):
@@ -10,9 +13,28 @@ class TestHTTP(testing.TestCase):
         super(self.__class__, self).setUp()
         self.api = api
 
-    def test_post(self) -> None:
-        result = self.simulate_post('/').json
-        self.assertIn('id', result.keys())
+    def test_http_estimate(self) -> None:
+        post_result = self.simulate_post('/').json
+        self.assertIn('id', post_result.keys())
+        id_ = post_result['id']
+
+        sentence = '特に１Ｆのバーは最高'
+        test_words = list(Word.from_sentence(sentence, MeCab()))
+        key = Key.from_words(test_words)
+
+        for k in key:
+            params = {
+                'id': id_,
+                'key': str(k)
+            }
+            result = self.simulate_put('/', params=params)
+            self.assertLess(result.status_code, 400)
+
+        get_result = self.simulate_get('/')
+        self.assertLess(get_result.status_code, 400)
+
+        correct = Word.to_str(test_words)
+        self.assertEqual(correct, get_result.json['sentence'])
 
 
 if __name__ == '__main__':
