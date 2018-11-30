ef4910961fa3428e6ec55a6b114e18601141c6c1
wbydo <wbydo@users.noreply.github.com>
2018-11-30 14:34:32 +0900

    設定からスキャン速度変更（NaN周りの作り込み不足）

--- src/renderer/views/Configure.tsx
+++ src/renderer/views/Configure.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import {connect} from "react-redux";
 import {Dispatch} from "redux";
 
-import {Action, deactivateConfigure} from "../state/actions";
+import {Action, deactivateConfigure, setScanSpeed} from "../state/actions";
 import * as styles from "./Configure.css";
+
+interface StateProps {
+  scanSpeed: number;
+}
 
 interface DispatchProps {
   deactivateConfigure: () => void;
+  setScanSpeed: (scanSpeed: number) => void;
 }
 
-type Props = DispatchProps;
+type Props = StateProps & DispatchProps;
 
 class Configure extends React.Component<Props> {
   public render() {
@@ -17,18 +22,28 @@ class Configure extends React.Component<Props> {
       <div id="Configure" className={styles.frame}>
         <div className={styles.content}>
           <h1>環境設定</h1>
+          <input type="number" value={this.props.scanSpeed} onChange={this.handleChange}/>
           <button onClick={(event) => this.props.deactivateConfigure()}>Off</button>
         </div>
       </div>
     );
   }
+
+  private handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement;
+    const scanSpeed  = parseInt(target.value, 10); // 10進数の意
+    if ( !isNaN(scanSpeed) ) {
+      this.props.setScanSpeed(scanSpeed);
+    }
+  }
 }
 
 export default connect(
-  null,
+  (state: StateProps) => state,
   (dispatch: Dispatch<Action>): DispatchProps => {
     return {
       deactivateConfigure: () => dispatch(deactivateConfigure()),
+      setScanSpeed: (scanSpeed: number) => dispatch(setScanSpeed(scanSpeed)),
     };
   },
 )(Configure);
