a9aebf7d2679b70da6f4a6feebfc297fb9935f52
wbydo <wbydo@users.noreply.github.com>
2018-12-01 12:32:48 +0900

    typo

--- src/renderer/state/timer/types.ts
+++ src/renderer/state/timer/types.ts
@@ -10,5 +10,5 @@ export const SET_ACTIVE: "sanascan/timer/SET_ACTIVE"
 export const SET_SCAN_SPEED: "sanascan/timer/SET_SCAN_SPEED"
   = "sanascan/timer/SET_SCAN_SPEED";
 
-export const RUN_MIDDLEWARE: "sanasdan/timer/RUN_MIDDLEWARE"
-  = "sanasdan/timer/RUN_MIDDLEWARE";
+export const RUN_MIDDLEWARE: "sanascan/timer/RUN_MIDDLEWARE"
+  = "sanascan/timer/RUN_MIDDLEWARE";
