5ae4a5686dcc59c4ef02b127c4a91dfff21b1ae7
wbydo <wbydo@users.noreply.github.com>
2018-12-01 22:29:18 +0900

    add error code

--- src/renderer/state/actions.ts
+++ src/renderer/state/actions.ts
@@ -1,17 +1,20 @@
 import * as types from "./types";
 
 export interface Action {
+  error: boolean;
   type: string;
 }
 
 export const activateConfigure: () => Action = () => {
   return {
+    error: false,
     type: types.ACTIVATE_CONFIGURE_WINDOW,
   };
 };
 
 export const deactivateConfigure: () => Action = () => {
   return {
+    error: false,
     type: types.DEACTIVATE_CONFIGURE_WINDOW,
   };
 };
--- src/renderer/state/cursol/actions.ts
+++ src/renderer/state/cursol/actions.ts
@@ -1,11 +1,13 @@
 import * as types from "./types";
 
 export interface Action {
+  error: boolean;
   type: string;
 }
 
 export const increment: () => Action = () => {
   return {
+    error: false,
     type: types.INCREMENT,
   };
 };
