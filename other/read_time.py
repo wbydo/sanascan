548ba3ce388d4185822bf6af5f7503e7129163d4
wbydo <wbydo@users.noreply.github.com>
2018-12-19 04:16:21 +0900

    check LM0006 - LM0008

--- other/read_time.py
+++ other/read_time.py
@@ -7,9 +7,9 @@ from sanascan_backend.lang_model import LangModel
 
 def stop_watch(msg):
     def decorater(func):
-        def wrapper():
+        def wrapper(name):
             start = time.time()
-            func()
+            func(name)
             finish = time.time()
             print(f'{msg}: {finish - start}')
 
@@ -18,16 +18,22 @@ def stop_watch(msg):
 
 
 @stop_watch('テキストファイルの読み込み時間')
-def text_file():
-    with (Path.home() / 'arpa/LM0006.txt').open() as f:
+def text_file(name):
+    with (Path.home() / 'arpa' / name).with_suffix('.txt').open() as f:
         lm = LangModel(f.read())
 
 
 @stop_watch('arpaファイルの読み込み時間')
-def arpa_file():
-    with (Path.home() / 'arpa/LM0006.pickle').open('rb') as f:
+def arpa_file(name):
+    with (Path.home() / 'arpa' / name).with_suffix('.pickle').open('rb') as f:
         lm = pickle.load(f)
 
+
 if __name__ == '__main__':
-    text_file()
-    arpa_file()
+    for name in ['LM0006', 'LM0007', 'LM0008']:
+        print(name)
+        print('--------------------------------')
+        text_file(name)
+        arpa_file(name)
+        print()
+        print()
