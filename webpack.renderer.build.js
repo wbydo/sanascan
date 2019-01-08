5f3ae5a69bc349363a37a76fe38ed9d1892f3347
wbydo <wbydo@users.noreply.github.com>
2019-01-08 15:25:08 +0900

    build設定整理

--- webpack.renderer.build.js
+++
@@ -1,8 +0,0 @@
-const webpack = require('webpack');
-const main = require('./webpack.main.js');
-const renderer = require('./webpack.renderer.js')
-
-module.exports = Object.assign(
-  renderer,
-  {target: 'web'}
-);
