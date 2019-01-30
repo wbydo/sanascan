14fea903d7e0342728cda57f3fe4a78b7c48ec02
wbydo <wbydo@users.noreply.github.com>
2019-01-31 00:09:30 +0900

    Positionをファイル分割

--- sanascan_backend/sanascan_backend/evaluation/node.py
+++ sanascan_backend/sanascan_backend/evaluation/node.py
@@ -1,39 +1,12 @@
 from typing import TYPE_CHECKING, Optional, Iterable
 
-from dataclasses import dataclass, replace, field
+from dataclasses import dataclass, field
 
 from .score import Score
+from .position import Position
 
 if TYPE_CHECKING:
     from .dp_matching import DPMatching
-
-
-@dataclass(init=True, repr=True, eq=True, frozen=True)
-class Position:
-    ref: int
-    est: int
-
-    def parent_positions(self) -> Iterable['Position']:
-        if self.ref - 1 >= 0 and self.est - 1 >= 0:
-            yield replace(self, ref=self.ref-1, est=self.est-1)
-        if self.ref - 2 >= 0 and self.est - 1 >= 0:
-            yield replace(self, ref=self.ref-2, est=self.est-1)
-        if self.ref - 1 >= 0 and self.est - 2 >= 0:
-            yield replace(self, ref=self.ref-1, est=self.est-2)
-
-    def penalty(self, other: 'Position') -> Score:
-        if (other.ref == self.ref - 1) and (other.est == self.est - 1):
-            penalty = Score()
-        elif (other.ref == self.ref - 2) and (other.est == self.est - 1):
-            penalty = Score(dropout=1)
-        elif (other.ref == self.ref - 1) and (other.est == self.est - 2):
-            penalty = Score(insert=1)
-        else:
-            raise Exception()
-        return penalty
-
-    def is_root(self) -> bool:
-        return self.ref == 0 and self.est == 0
 
 
 @dataclass(init=True, repr=False, eq=False, frozen=False)
